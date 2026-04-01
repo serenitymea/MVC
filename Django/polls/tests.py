365df468f7a662b9214fcfde2e2f781f3f416c08
serenity <toxayuo@gmail.com>
2026-04-02 01:56:03 +0200

    added tests Django

--- Django/polls/tests.py
+++ Django/polls/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.utils import timezone
+import datetime
+from .models import Question, Choice
 
-# Create your tests here.
+
+class QuestionModelTest(TestCase):
+
+    def test_create_question(self):
+        q = Question.objects.create(
+            question_text="t question",
+            pub_date=timezone.make_aware(datetime.datetime(2024, 1, 1))
+        )
+        self.assertEqual(q.question_text, "t question")
+
+
+class ChoiceModelTest(TestCase):
+
+    def test_create_choice(self):
+        q = Question.objects.create(
+            question_text="t question",
+            pub_date=timezone.make_aware(datetime.datetime(2024, 1, 1))
+        )
+
+        c = Choice.objects.create(
+            question=q,
+            choice_text="t choice",
+            votes=0
+        )
+
+        self.assertEqual(c.choice_text, "t choice")
+        self.assertEqual(c.votes, 0)
