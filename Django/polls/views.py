379b5f304e6c643f272c3cfbeee1c466feef69c7
serenity <toxayuo@gmail.com>
2026-04-10 15:39:46 +0200

    Add correct tests and fix bugs

--- Django/polls/views.py
+++ Django/polls/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
 from .models import Question
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from django.utils import timezone
+from django.views import generic
 
-def index(request):
-    latest_question_list = Question.objects.all()
-    return render(request, "polls/index.html", {
-        "latest_question_list": latest_question_list
-    })
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by("-pub_date")
     
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
